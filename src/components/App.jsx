f5486dcde7ed89acfbaaf2fa5e3f75e03c907ce3
taranvd <apple.vlad.taran@gmail.com>
2023-08-12 07:49:32 +0300

    finish all task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,68 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactsList } from './ContactsList/ContactsList';
+import { ContactFilter } from './ContactFilter/ContactFilter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  changeFilter = newFilter => {
+    this.setState({
+      filter: newFilter,
+    });
+  };
+
+  addContact = newContact => {
+    const existingContact = this.state.contacts.find(
+      contact => contact.name === newContact.name
+    );
+
+    if (existingContact) {
+      alert(`${newContact.name} вже є у списку контактів!`);
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, newContact],
+      };
+    });
+  };
+
+  deleteContact = idContact => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== idContact
+        ),
+      };
+    });
+  };
+
+  render() {
+    const { filter, contacts } = this.state;
+
+    const visibleContacts = contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(filter.toLowerCase());
+    });
+
+    return (
+      <>
+        <Section title="Phone Book">
+          <ContactForm addContact={this.addContact} />
+        </Section>
+        <Section title="Contact">
+          <ContactFilter filter={filter} changeFilter={this.changeFilter} />
+          <ContactsList
+            contacts={visibleContacts}
+            deleteContact={this.deleteContact}
+          />
+        </Section>
+      </>
+    );
+  }
+}
---
+++ src/components/ContactsList/ContactsList.style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 10px;
+  padding: 0;
+  list-style: none;
+`;
+
+export const ListItem = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
